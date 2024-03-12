7827d670a1e2568b354e46057da1519458c735ab
yaak <sashayanochkin@gmail.com>
2024-03-12 14:18:25 +0300

    fix tsx

--- lab_01_18/src/components/buttons/Button.jsx
+++
@@ -1,9 +0,0 @@
-import "./Button.css";
-
-export function Button({ children, isActive, ...props }) {
-  return (
-    <button {...props} className={`${isActive ? "button active" : "button"} ${props.className}`}>
-      {children}
-    </button>
-  );
-}
--- lab_01_18/src/contexts/ThemeContext.jsx
+++
@@ -1,9 +0,0 @@
-import React, { createContext, useContext } from "react";
-
-const ThemeContext = createContext();
-
-export const ThemeProvider = ({ children, value }) => (
-  <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
-);
-
-export const useTheme = () => useContext(ThemeContext);
